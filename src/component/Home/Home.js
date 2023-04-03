453a12fc0838ccd317079c34f63a0a8f6d52dd28
BayanBanat <bayankbanat99@gmail.com>
2023-04-03 08:17:54 +0000

    class 16 2

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,14 +1,10 @@
 import Header from "../Header/Header.js";
 import Tours from "../Tours/Tours.js";
 import Footer from "../Footer/Footer.js";
-// import App from "../../App.js";
+
 import './Home.css';
 
-// const toursData = require("../../data/data.json");
-
-
 function Home(props) {
-    // console.log(props);
     const dataRecieved=props.data;
     return (
         <>
